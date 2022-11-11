ea706109955c992cbbea276a08144ca24c5a0b7f
charansuryatejagoud <charansuryatejagoud@gmail.com>
2022-11-11 12:59:56 +0530

    update

--- schemas/Homepage/Group.ts
+++ schemas/Homepage/Group.ts
@@ -32,11 +32,43 @@ export default{
             group: 'main'
         },
         {
+            title: 'Demo Variant',
+            name: 'demoVariant',
+            type: 'string',
+            group: 'main',
+            options:{
+                list:[
+                ]
+        }
+    },
+        {
             title: 'Large Variant',
             name: 'largeVariant',
             type: 'string',
             group: 'main'
         },
+        {
+            title: 'Demo Large Variant',
+            name: 'demoLargeVariant',
+            type: 'string',
+            group: 'main',
+            options:{
+                list:[
+                    { title: 'two-row-grid', value: 'two-row-grid' },
+                    { title: 'single-card-carousel-with-bg-image', value: 'single-card-carousel-with-bg-image'},
+                    { title: 'carousel-with-award-cards', value: 'carousel-with-award-cards'},
+                    { title: 'multi-cards-carousel-with-bg-image', value: 'multi-cards-carousel-with-bg-image' },
+                    { title: '3-column-grid-button-with-link', value: '3-column-grid-button-with-link' },
+                    { title: 'media-card', value: 'media-card' },
+                    { title: 'carousel-with-focused-title', value: 'carousel-with-focused-title' },
+                    { title: 'group-with-information', value: 'group-with-information' },
+                    { title: 'group-with-links', value: 'group-with-links' },
+                    { title: 'group-with-maps', value: 'group-with-maps' },
+                    { title: 'card-with-right-aligned-content', value: 'card-with-right-aligned-content' },
+                    { title: 'card-with-focused-title', value: 'card-with-focused-title' },   
+                ]
+        },
+    },
         {
             title: 'Sub Title',
             name: 'subTitle',
